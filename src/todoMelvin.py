9b00f65239cb6a950822c8567573dbea938ba6f3
p4r4digm <btownsen@dapperhat.net>
2013-08-13 18:55:48 -0700

    First whack at cleaning up melvin and making him more usable.  You can now specify number of repos to pull from a single run

--- src/todoMelvin.py
+++ src/todoMelvin.py
@@ -4,35 +4,54 @@ from subprocess import call
 from pygithub3 import Github
 
 from db.todoRedis import connect
-import db.todoRepos
+from db.todoRepos import repoExists, addNewRepo
 
-def tagNewRepo(username, password):
-    gh = Github(login=username, password=password)
 
-    #Get lsit of repos from recently pushed pubic list
-    repoList = []
-    
-    for event in gh.events.list().iterator():
-        if event.type == 'PushEvent':
-            repoList.append(event.repo)
-            
-    chosenRepo = None
-    username = ''
-    reponame = ''
-    
-    while not chosenRepo:
-        repo = repoList[random.randint(0, len(repoList) - 1)]
-        username = repo.name.split('/')[0]
-        reponame = repo.name.split('/')[1]
+def checkForValidEvent(gh, event):    
+    if event.type == 'PushEvent':
+        repo = event.repo
+        username = repo.name.split('/', 1)[0]
+        reponame = repo.name.split('/', 1)[1]
         
-        if not db.todoRepos.repoExists(username, reponame):
+        if not repoExists(username, reponame):
             try:
                 chosenRepo = gh.repos.get(username, reponame)
+                if chosenRepo.has_issues:
+                    return chosenRepo
             except:
-                continue
+                pass
             
-    db.todoRepos.addNewRepo(username, reponame)
-    call(['git', 'clone', chosenRepo.git_url])
+    return None
+    
+def findRepos(gh, count):
+    repoList = []
+    
+    if count <= 0: return repoList
+    
+    for event in gh.events.list().iterator():
+        repo = checkForValidEvent(gh, event)
+        
+        if repo:
+            repoList.append(repo)
+            if len(repoList) == count: return repoList
+            
+    return repoList
+            
+    
+    
+
+#grabs an amount of repos recently pushed to, checks them out and adds them to redis
+#Takes a pygithub3.Github object (auhtenticated) and how many repos to pull at once
+def tagNewRepos(gh, count):         
+            
+    #find random shiny new repo that allows issues and hasnt been tagged before
+    chosenRepo = findRepos(gh, count)
+    
+    #Add repo to Redis
+    redisRepo = addNewRepo(chosenRepo.owner.login, chosenRepo.name)
+    
+    
+    call(['git', 'clone', chosenRepo.git_url, 'repos/%s' % (redisRepo.key())])
             
     
               
