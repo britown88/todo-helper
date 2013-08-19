801c3c0ba9502ea682bd9553cf94a8337d95e578
Peter Conerly <pconerly@gmail.com>
2013-08-19 10:35:47 -0700

    fingers crossed

--- src/findTodo.py
+++ src/findTodo.py
@@ -1,7 +1,8 @@
 import os
 
 from pygments import highlight
-from pygments.lexers import PythonLexer
+from pygments.lexers import PythonLexer 
+from pygments.lexers import guess_lexer, guess_lexer_for_filename
 from pygments.formatter import Formatter
 
 ## custom formatter
@@ -19,8 +20,39 @@ class NullFormatter(Formatter):
 def parse(codeInput):
     return highlight(codeInput, PythonLexer(), NullFormatter())
 
+def walk(dir):
+    for dirname, dirnames, filenames in os.walk(dir):
+        # print path to all subdirectories first.
+        for subdirname in dirnames:
+            print os.path.join(dirname, subdirname)
+
+        # print path to all filenames.
+        for filename in filenames:
+            print os.path.join(dirname, filename)
+            fin = open(os.path.join(dirname, filename))
+            code = fin.read()
+            print parse(code)
+
+        # Advanced usage:
+        # editing the 'dirnames' list will stop os.walk() from recursing into there.
+        if '.git' in dirnames:
+            # don't go into any .git directories.
+            dirnames.remove('.git')
+
+
 
 if __name__ == '__main__':
+    testDir = os.path.join(
+        PROJECT_PATH,
+        '..',
+        'test',
+        'fixtures')
+    walk(testDir)
+
+    # for fin in os.listdir(testDir):
+    #     pass
+
+
     fin = open(os.path.join(
         PROJECT_PATH,
         '..',
@@ -35,3 +67,9 @@ if __name__ == '__main__':
 
     # For good fun, try:
     # src $ python findTodo.py 
+
+
+
+
+
+
